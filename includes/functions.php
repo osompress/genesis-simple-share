13ec83413c9104c693bafcebbee45d2f5a9577ff
Marcos Schratzenstaller <marksabbath@gmail.com>
2019-03-11 17:31:45 -0300

    Files restructuration.

--- includes/functions.php
+++ includes/functions.php
@@ -2,16 +2,12 @@
 /**
  * General functions that may be needed in admin or in the front end.
  *
- *
  * @category Genesis Simple Sharrre
  * @package  Functions
  * @author   copyblogger
  * @license  http://www.opensource.org/licenses/gpl-license.php GPL-2.0+
  */
- 
- //add functions that are used in admin and front end of the site
- 
-//* Prevent direct access to the plugin
+
 if ( ! defined( 'ABSPATH' ) ) {
-    die( __( 'Sorry, you are not allowed to access this page directly.', 'genesis-simple-share' ) );
+	die( esc_html__( 'Sorry, you are not allowed to access this page directly.', 'genesis-simple-share' ) );
 }
